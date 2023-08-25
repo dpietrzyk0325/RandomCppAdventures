74e64840cb439022cd9ab55c6f279ee09347717d
dpietrzyk0325 <dpietrzyk5202@conestogac.on.ca>
2023-08-24 20:22:44 -0400

    docs: am I really willing to do this at this stage?

--- OpenCV Face Detection/main.cpp
+++ OpenCV Face Detection/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// I will be honest, I have no idea how to use opencv in C/C++, but this will be an interesting project
+// I will be honest, I have no idea how to use opencv in C/C++, but this would be an interesting project
 
 int main()
 {
