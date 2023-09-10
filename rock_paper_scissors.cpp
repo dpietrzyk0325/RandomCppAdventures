45858ab223581b9153bf27c4a62624eae94dab92
dpietrzyk0325 <dpietrzyk5202@conestogac.on.ca>
2023-09-10 15:03:17 -0400

    add: rock_paper_scissors.cpp

--- rock_paper_scissors.cpp
+++ rock_paper_scissors.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
 
-// funtion prototypes
-std::string getSelection(player player);
-void setSelection(player &player, char selection);
-
-struct player
+// Struct Player
+struct Player
 {
+    // members
     std::string name; // used for the name of the player
     char selection; // used for setting a selection
 };
 
-std::string getSelection(player player){
-    return player.name;
-};
+// function prototypes
 
-void setSelection(player &player, char selection){
+std::string getSelection(Player player);
+void setSelection(Player &player, char selection);
+
+/*  
+    Author: Dawid Pietrzyk
+    Date Created: 
+    Input: Structure variable of type Player
+    Output: The name member of the input structure variable of type Player  
+*/
+std::string getSelection(Player player){
+    return player.name;
+}
+
+/*  
+    Author: Dawid Pietrzyk
+    Date Created: 
+    Input: Structure variable of type Player, and character variable containing the selection
+    Output: None (void) 
+*/
+void setSelection(Player &player, char selection){
     player.name = selection;
 }
 
+// main function
 int main()
 {
-    srand(time(0));
-    int randNum = (rand() % 3) + 1;
+    srand(time(0)); // seed random number generator
+    int randNum = (rand() % 3) + 1; 
     
-    player human;
-    player computer;
+    Player human; // make a structure variable of type Player for the human player
+    Player computer; // make a structure variable of type player for the computer
 
+    std::cout << "******************************************************\n";
+    std::cout << "***************ROCK PAPER SCISSORS GAME***************\n";
+    std::cout << "******************************************************\n";
+    std::cout << "Please make a selection: \n";
     return 0;
 }
