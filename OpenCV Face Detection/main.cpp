89852233d8f30c73f96b54375182631172f5b3dc
dpietrzyk0325 <dpietrzyk5202@conestogac.on.ca>
2023-09-16 09:37:22 -0400

    docs: formatting

--- OpenCV Face Detection/main.cpp
+++ OpenCV Face Detection/main.cpp
@@ -2,7 +2,7 @@
 
 // I will be honest, I have no idea how to use opencv in C/C++, but this would be an interesting project
 
-// I think currently with school and other projects I have to do, I will not be able to do this project 
+//      I think currently with school and other projects I have to do, I will not be able to do this project 
 // right now but I will come back to it later in future (there will be a lot to this project)
 
 int main()
