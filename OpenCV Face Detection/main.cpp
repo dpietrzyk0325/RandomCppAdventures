92fffdfc6ec357e8b730df4eaaf48b088d83f1fd
dpietrzyk0325 <dpietrzyk5202@conestogac.on.ca>
2023-09-16 09:34:56 -0400

    docs: talked about leaving the OpenCV project for future

--- OpenCV Face Detection/main.cpp
+++ OpenCV Face Detection/main.cpp
@@ -2,6 +2,9 @@
 
 // I will be honest, I have no idea how to use opencv in C/C++, but this would be an interesting project
 
+// I think currently with school and other projects I have to do, I will not be able to do this project 
+// right now but I will come back to it later in future (there will be a lot to this project)
+
 int main()
 {
     
